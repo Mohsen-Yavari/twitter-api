7b4d8ea9a587cec4bce9dfeeebbd447598b51e46
Mohsen-Yavari <mohsenyavari713@gmail.com>
2022-07-14 13:02:45 +0330

    Articale number show withe use state 3 items

--- components/Wedgets.js
+++ components/Wedgets.js
@@ -1,8 +1,10 @@
 import { SearchIcon } from "@heroicons/react/solid";
 import News from "./News";
+import {useState} from "react";
 
 
 export default function Wedgets({newsResults}) {
+  const [articleNumber,setArticleNumber] = useState(3);
   return (
     <div className="xl:w-[600px] hidden lg:inline mr-8 space-y-5">
       <div className="w-[90%]   top-0 bg-white py-2 z-50">
@@ -14,7 +16,7 @@ export default function Wedgets({newsResults}) {
 
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
       <h4 className="font-bold text-xl px-4">مقالات انگلیسی</h4>
-      {newsResults.map((article)=>(
+      {newsResults.slice(0,articleNumber).map((article)=>(
         <News key={article.tittle} article={article} />
       ))}
       <button className="text-blue-300 pr-3 pb-3 hover:text-blue-400">بیشتر</button>
